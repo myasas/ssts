6cbf1f08b76e32983398116e4de72633ff72a345
turing <myasas@gmail.com>
2015-08-26 08:04:21 +0530

    Added Model to the software

---
+++ src/main/java/model/Feedback.java
@@ -0,0 +1,94 @@
+package model;
+
+public class Feedback {
+	
+	private String fbID="fb_id";	
+	private String userID="user_id";
+	private String fbType="fb_type";
+	private String fbCategory="fb_category";
+	private String message="message";
+
+//	Getters and Setters
+	/**
+	 * @return the fbID
+	 */
+	public String getFbID() {
+		return fbID;
+	}
+
+
+	/**
+	 * @param fbID the fbID to set
+	 */
+	public void setFbID(String fbID) {
+		this.fbID = fbID;
+	}
+
+
+	/**
+	 * @return the userID
+	 */
+	public String getUserID() {
+		return userID;
+	}
+
+
+	/**
+	 * @param userID the userID to set
+	 */
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+
+	/**
+	 * @return the fbType
+	 */
+	public String getFbType() {
+		return fbType;
+	}
+
+
+	/**
+	 * @param fbType the fbType to set
+	 */
+	public void setFbType(String fbType) {
+		this.fbType = fbType;
+	}
+
+
+	/**
+	 * @return the fbCategory
+	 */
+	public String getFbCategory() {
+		return fbCategory;
+	}
+
+
+	/**
+	 * @param fbCategory the fbCategory to set
+	 */
+	public void setFbCategory(String fbCategory) {
+		this.fbCategory = fbCategory;
+	}
+
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+
+
+
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,114 @@
+package model;
+
+public class User {
+	
+	private String userID="user_id";
+	private String userType="user_type";
+	private String userLogin="user_login";
+	private String userPass="user_password";
+	private String userStatus="user_status";
+	private String userName="user_name";
+	private String userBday="user_bday";	
+	private String userReg="user_reg";
+
+
+	
+//	Getters and Setters
+	/**
+	 * @return the userID
+	 */
+	public String getUserID() {
+		return userID;
+	}
+	/**
+	 * @param userID the userID to set
+	 */
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	/**
+	 * @return the userType
+	 */
+	public String getUserType() {
+		return userType;
+	}
+	/**
+	 * @param userType the userType to set
+	 */
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	/**
+	 * @return the userLogin
+	 */
+	public String getUserLogin() {
+		return userLogin;
+	}
+	/**
+	 * @param userLogin the userLogin to set
+	 */
+	public void setUserLogin(String userLogin) {
+		this.userLogin = userLogin;
+	}
+	/**
+	 * @return the userPass
+	 */
+	public String getUserPass() {
+		return userPass;
+	}
+	/**
+	 * @param userPass the userPass to set
+	 */
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+	/**
+	 * @return the userStatus
+	 */
+	public String getUserStatus() {
+		return userStatus;
+	}
+	/**
+	 * @param userStatus the userStatus to set
+	 */
+	public void setUserStatus(String userStatus) {
+		this.userStatus = userStatus;
+	}
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	/**
+	 * @return the userBday
+	 */
+	public String getUserBday() {
+		return userBday;
+	}
+	/**
+	 * @param userBday the userBday to set
+	 */
+	public void setUserBday(String userBday) {
+		this.userBday = userBday;
+	}
+	/**
+	 * @return the userReg
+	 */
+	public String getUserReg() {
+		return userReg;
+	}
+	/**
+	 * @param userReg the userReg to set
+	 */
+	public void setUserReg(String userReg) {
+		this.userReg = userReg;
+	}
+	
+}
