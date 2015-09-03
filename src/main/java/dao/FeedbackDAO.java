01893de8f1f514c8c4392440a02941a21c509709
turing <myasas@gmail.com>
2015-09-04 02:26:33 +0530

    Added UserReports and implemented Feedback

--- src/main/java/dao/FeedbackDAO.java
+++ src/main/java/dao/FeedbackDAO.java
@@ -1,6 +1,11 @@
 package dao;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import data.DatabaseConnection;
 import model.Feedback;
@@ -32,14 +37,83 @@ public class FeedbackDAO {
 		return feedback;
 	}
 	
-//	public static void main(String[] args) {
-//	Feedback fbd = new Feedback();
-//	fbd.setFbID("1");
-//	
-//	FeedbackDAO fbd2 = new FeedbackDAO();
-//	fbd = fbd2.getFeedbackBySettingValues(fbd);
+	
+	   public void addFeedback(Feedback feedback) {
+		   String SQLInsert = ("insert into feedback(user_id, fb_type, fb_category, message) values (?, ?, ?, ?)");		   
+	        try {
+	            PreparedStatement preparedStatement = DatabaseConnection.getInstance().getConnection().prepareStatement(SQLInsert);
+	            // Parameters start with 1
+	            preparedStatement.setString(1, feedback.getUserID());    	            
+	            preparedStatement.setString(2, feedback.getFbType());    
+	            preparedStatement.setString(3, feedback.getFbCategory());  
+	            preparedStatement.setString(4, feedback.getMessage());      	            
+	            preparedStatement.executeUpdate();
+
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        } catch (Exception e) {
+	            e.printStackTrace();
+			}
+	    }
+	   
+	    public List<Feedback> getAllFeedbacks() {
+	        List<Feedback> feedbacks = new ArrayList<Feedback>();
+	        try {
+	            Statement statement =  DatabaseConnection.getInstance().getConnection().createStatement();
+	            ResultSet rs = statement.executeQuery("SELECT fb_id, user_id,(SELECT user_name FROM ssts.users where user_id= feedback.user_id) , fb_type, fb_category, message, updateTime FROM ssts.feedback");
+	            while (rs.next()) {
+	            	Feedback feedback = new Feedback();
+	            	feedback.setFbID(rs.getString("fb_id"));
+	            	feedback.setFbType(rs.getString("fb_type"));
+	            	feedback.setFbCategory(rs.getString("fb_category"));
+	            	feedback.setMessage(rs.getString("message"));
+	            	feedback.setUserID(rs.getString("user_id"));	            	
+	            	feedback.setUserName(rs.getString("(SELECT user_name FROM ssts.users where user_id= feedback.user_id)"));	            	
+	            	feedback.setUpdateTime(rs.getString("updateTime"));	
+	                
+	            	feedbacks.add(feedback);
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        } catch (Exception e) {
+				e.printStackTrace();
+			}
+
+	        return feedbacks;
+	    }
+	    
+	    public Feedback getFeedbackById(String feedbackID) {
+	        Feedback feedback = new Feedback();
+	        try {
+	            PreparedStatement preparedStatement = DatabaseConnection.getInstance().getConnection().prepareStatement("select * from feedback where fb_id=?");
+	            preparedStatement.setString(1, feedbackID);
+	            ResultSet rs = preparedStatement.executeQuery();
+
+	            if (rs.next()) {
+	               	feedback.setFbID(rs.getString("fb_id"));            	
+	            	feedback.setFbType(rs.getString("fb_type"));
+	            	feedback.setFbCategory(rs.getString("fb_category"));
+	            	feedback.setUserID(rs.getString("user_id"));	
+	            	feedback.setMessage(rs.getString("message"));
+	            	feedback.setUpdateTime(rs.getString("updateTime"));			
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        } catch (Exception e) {
+				e.printStackTrace();
+			}
+	        return feedback;
+	    }	    
+	    
+//	    public static void main(String[] args) {
+//			Feedback fb = new Feedback();
+////			fb.setUserID("16");
+////			fb.setFbType("Idea");
+////			fb.setMessage("Idea TestDB");
+////			fb.setMessage("Message 123456");
+//			FeedbackDAO dbDao = new FeedbackDAO();
+//			System.out.println(dbDao.getFeedbackById("23").getUpdateTime());
 //
-//	System.out.println(fbd.getMessage());
-//}
+//		}
 	
 }
--- src/main/java/model/Feedback.java
+++ src/main/java/model/Feedback.java
@@ -3,10 +3,12 @@ package model;
 public class Feedback {
 	
 	private String fbID="fb_id";	
-	private String userID="user_id";
 	private String fbType="fb_type";
 	private String fbCategory="fb_category";
 	private String message="message";
+	private String userID="user_id";
+	private String userName="(SELECT user_name FROM ssts.users where user_id= feedback.user_id)";		
+	private String updateTime="updateTime";
 
 //	Getters and Setters
 	/**
@@ -86,6 +88,38 @@ public class Feedback {
 	 */
 	public void setMessage(String message) {
 		this.message = message;
+	}
+
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+	/**
+	 * @return the updateTime
+	 */
+	public String getUpdateTime() {
+		return updateTime;
+	}
+
+
+	/**
+	 * @param updateTime the updateTime to set
+	 */
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
 	}
 	
 
