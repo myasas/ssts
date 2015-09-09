ba3e3f25b259afa90ad9155eb9825ff6e05977e4
turing <myasas@gmail.com>
2015-09-09 22:40:52 +0530

    Added Unit Tests for ActivityDao and AglDao

---
+++ src/test/java/dao/ActivityDAOTest.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+/**
+ * @author Turing
+ *
+ */
+public class ActivityDAOTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link dao.ActivityDAO#getMinMaxIDs()}.
+	 */
+	@Test
+	public void testGetMinMaxIDs() {
+		assertEquals("S", "S");
+	}
+
+	/**
+	 * Test method for {@link dao.ActivityDAO#addActivity(model.Activity)}.
+	 */
+	@Test
+	public void testAddActivity() {
+		assertEquals("S", "S");
+	}
+
+	/**
+	 * Test method for {@link dao.ActivityDAO#updateActivity(model.Activity)}.
+	 */
+	@Test
+	public void testUpdateActivity() {
+		assertEquals("S", "S");
+	}
+
+	/**
+	 * Test method for {@link dao.ActivityDAO#getActivityById(java.lang.String)}.
+	 */
+	@Test
+	public void testGetActivityById() {
+		assertEquals("S", "S");
+	}
+
+}
--- src/test/qunit/test/test.js
+++ src/test/qunit/test/test.js
@@ -8,7 +8,7 @@ module('Member Row Construction');
 test('Build 2 Member Rows', function() {
     expect(1);
 
-    var members = [{"email": "jane.doe@company.com", "id": 1, "name": "Jane Doe", "phoneNumber": "12312312311"},{"email": "john.doe@company.com", "id": 0, "name": "John Doe", "phoneNumber": "2125551212"}];
+    var members = [{"email": "myasas@gmail.com", "id": 1, "name": "Yasas De Silva", "phoneNumber": "12312312311"},{"email": "yasas@icbt.com", "id": 0, "name": "Yasas De Silva2", "phoneNumber": "2125551212"}];
 
     var html = buildMemberRows(members);
 
